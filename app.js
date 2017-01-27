220daad9a0b12d8c52287cc1becafb22f414bcce
ryskiz <ryan.skinner1193@gmail.com>
2017-01-27 17:50:31 -0500

    styles up and running

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 // routing
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/public/stylesheets/'));
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
