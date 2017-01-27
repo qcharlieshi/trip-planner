c3d7d4cf9e453b968921ddf0eb63666e0bb9bda4
ryskiz <ryan.skinner1193@gmail.com>
2017-01-27 17:22:14 -0500

    everything is now up and running

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // routing
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
